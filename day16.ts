c624f3fd986efbf64f4a8d5f69384b27db2e7608
Tyler Knapp <knapptr@gmail.com>
2024-12-16 20:26:03 -0500

    Solves 16 part 1

--- day16.ts
+++ day16.ts
@@ -10,7 +10,7 @@ interface Reindeer{
     direction: Dir
     coords: Coord
     score: number;
-    visitedCoords: Set<string>
+    path: string[];
 }
 
 const cloneDeer = (deer:Reindeer):Reindeer => {
@@ -18,7 +18,7 @@ const cloneDeer = (deer:Reindeer):Reindeer => {
         direction: deer.direction,
         coords: [...deer.coords],
         score: deer.score,
-        visitedCoords: new Set([...deer.visitedCoords])
+        path: [...deer.path]
     }
 }
 
@@ -37,33 +37,6 @@ const turnDirections: {[k in Dir]: [Dir,Dir,Dir]} = {
 }
 const TURNSCORE = 1000;
 const MOVESCORE = 1;
-const turnDeer = (deer:Reindeer,direction:Dir):Reindeer=> {
-   const [turnDirIndex,score] = direction === "LEFT" ? [0,TURNSCORE] : direction === "DOWN"? [2,TURNSCORE * 2]: [1, TURNSCORE] ;
-    deer.direction = turnDirections[deer.direction][turnDirIndex];
-    deer.score += score;
-    return cloneDeer(deer)
-
-}
-const advanceDeer = (deer:Reindeer): Reindeer =>{
-    const [dY,dX] = moveDeltas[deer.direction];
-    deer.visitedCoords.add(coordToString(deer.coords))
-    deer.coords = [deer.coords[0] + dY, deer.coords[1] + dX ];
-    deer.score += MOVESCORE;
-    return cloneDeer(deer)
-}
-/** clones deer and moves, adjusting score as nesc */
-const possibleDeerMoves = (deer:Reindeer):[Reindeer,Reindeer,Reindeer,Reindeer] => {
-    // Deer can continue forward
-    // Deer can turn 90 left, then move forward
-    // Deer can turn 90 right, then move forward
-    // Deer could turn 180 (2 90 deg turns) then move forward
-    
-    const backDeer = turnDeer(cloneDeer(deer),"DOWN");
-    const leftDeer = turnDeer(cloneDeer(deer),"LEFT");
-    const rightDeer = turnDeer(cloneDeer(deer),"RIGHT");
-    return [advanceDeer(deer), advanceDeer(leftDeer), advanceDeer(rightDeer), advanceDeer(backDeer)];
-}
-
 const moveDeltas:{[k in Dir]: Coord} = {
     "UP": [-1,0],
     "DOWN": [1,0],
@@ -71,51 +44,148 @@ const moveDeltas:{[k in Dir]: Coord} = {
     "RIGHT": [0,1],
 }
 
-const initDeer = ():Reindeer => {return {direction: "RIGHT", coords: [-1,-1], score: 0, visitedCoords: new Set}}
-const globalDeer = initDeer();
+const initDeer = (coords:Coord):Reindeer => {return {direction: "RIGHT", coords, score: 0,path:[]}}
+
+const advanceDeer = (deer:Reindeer):Reindeer => {
+    const newDeer = cloneDeer(deer);
+    const [dY,dX] = moveDeltas[deer.direction];
+    newDeer.path.push(coordToString(deer.coords));
+    newDeer.coords = [deer.coords[0] + dY, deer.coords[1] +dX];
+    newDeer.score += MOVESCORE;
+    return newDeer;
+}
+
+const turnDeer = (deer:Reindeer, direction:Dir):Reindeer =>{
+    const newDeer = {...deer};
+    const turnIndex = direction === "LEFT"?0:direction=== "RIGHT"?1:2;
+    newDeer.direction = turnDirections[newDeer.direction][turnIndex];
+    newDeer.score += TURNSCORE;
+    return newDeer;
+}
+
 
 type Maze = string[][];
-const parseMaze  = (input:string) =>  parseGrid2D(input, (char,ri,ci)=>{
-    if (char === "S"){globalDeer.coords = [ri,ci]}
+const parseMaze  = (input:string):[Maze,Coord,Coord] =>  {
+    let deerCoords:Coord = [-1,-1];
+    let endCoords:Coord = [-1,-1];
+    const cells = parseGrid2D(input, (char,ri,ci)=>{
+    if (char === "S"){deerCoords = [ri,ci];}
+    if(char ==="E"){ endCoords = [ri,ci];}
     return char})
+    return [cells,deerCoords,endCoords]
+}
 
+const getAtCoords = (maze:Maze,coords:Coord):string =>{
+    return maze[coords[0]][coords[1]];
+}
+const setCoordMin = (map: number[][],coord:Coord,value:number)=> {
+    const currentValue = map[coord[0]][coord[1]];
+    map[coord[0]][coord[1]] = Math.min(currentValue,value);
+}
 
-let globalLowest = Infinity
-const globalSeen: Map<string,number> = new Map;
-const solveOne = (maze:Maze,deer:Reindeer):number | null =>{
-    if(deer.score > globalLowest){return null}
-    const mazeCell = maze[deer.coords[0]][deer.coords[1]];
-    // base case deer is at end 
-    if (mazeCell === "E"){ 
-        console.log("AT END!");
-        return deer.score;}
-    // Deer is at wall. Cant continue
-    if (mazeCell === "#"){return null}
-    // Deer is at seen space, abort
-    const oldScore = globalSeen.get(coordToString(deer.coords)) || Infinity;
-    if(globalSeen.has(coordToString(deer.coords)) && oldScore < deer.score){
-        return null 
+type DQueue = Reindeer[]; 
+/** A poorly (slow) implemented priority queue */
+const priorityEnqueue = (cheapestQueue:DQueue, deer:Reindeer) =>{
+    cheapestQueue.push(deer);
+    cheapestQueue.sort((a,b)=> b.score - a.score);
+}
+
+const dijkstra = (maze:string[][],deer:Reindeer,distanceTracker: number[][],seenTracker: Set<string>):void =>{
+    const queue:DQueue= [deer]; // Priority queue, done lazily here
+
+    while(queue.length > 0){
+        const currentDeer = queue.pop()!;
+        // Handle end of path information
+        // console.log(currentDeer);
+    // Update values of all neighbors
+        const forwardDeer = advanceDeer(currentDeer);
+        const leftDeer = advanceDeer(turnDeer(currentDeer,"LEFT")) ;
+        const rightDeer = advanceDeer(turnDeer(currentDeer,"RIGHT")) ;
+        const backDeer = advanceDeer(turnDeer(currentDeer,"DOWN")) ;
+
+    const allNbors = [forwardDeer,leftDeer,rightDeer,backDeer];
+    for (const nbor of allNbors){
+        // console.log({nbor});
+        if(getAtCoords(maze,nbor.coords) !== "#"){
+            setCoordMin(distanceTracker,nbor.coords,nbor.score);
+        }
+        // add unseen coords to pQueue
+        if(!seenTracker.has(coordToString(nbor.coords)) && getAtCoords(maze,nbor.coords) !== "#"){
+            priorityEnqueue(queue,nbor);
+        }
     }
-    globalSeen.set(coordToString(deer.coords), deer.score);
-    // Deer must be an an empty space. 
-    // check for all 4 posssible deer
-    const moves = possibleDeerMoves(deer);
-    for (const dirDeer of moves ){
-        const res = solveOne(maze,dirDeer);
-        if(res!== null){globalLowest = Math.min(res,globalLowest)}
+        seenTracker.add(coordToString(currentDeer.coords));
+    }
+}
+
+const one = (input:string):number => {
+    const [maze,deerCoords,endCoords] = parseMaze(input);
+    const deer = initDeer(deerCoords);
+    // Setup seen array
+    const seen = new Set<string>;
+    // Setup Length Measurements
+    const initRow = Array.from({length:maze[0].length}, ()=>Infinity);
+    const distanceArray:number[][] = Array.from({length: maze.length}, ()=>[...initRow]);
+    // set start distance to 0
+    distanceArray[deerCoords[0]][deerCoords[1]] = 0;
+    // Visit shortest
+    dijkstra(maze,deer,distanceArray,seen);
+    return distanceArray[endCoords[0]][endCoords[1]];
+}
+const two = (input:string):number => {
+    const [maze,deerCoords,endCoords] = parseMaze(input);
+    const deer = initDeer(deerCoords);
+    // Setup seen array
+    const seen = new Set<string>;
+    // Setup Length Measurements
+    const initRow = Array.from({length:maze[0].length}, ()=>Infinity);
+    const distanceArray:number[][] = Array.from({length: maze.length}, ()=>[...initRow]);
+    // set start distance to 0
+    distanceArray[deerCoords[0]][deerCoords[1]] = 0;
+    // Visit shortest
+    dijkstra(maze,deer,distanceArray,seen);
+    //bfs the distances?
+    // Walk from end to start, always decreasing value
+    const cells = [];
+    let queue = [endCoords];
+    const visited = new Set;
+while (queue.length > 0){
+    let currentCoords = queue[0];
+    visited.add(coordToString(currentCoords));
+    queue = queue.slice(1);
+    let lowestMoves:Coord[] = [];
+    let lowestCost = Infinity;
+    for (const [dY,dX] of Object.values(moveDeltas)){
+        const targetCoords:Coord = [currentCoords[0] + dY,currentCoords[1] + dX];
+        const cost = distanceArray[targetCoords[0]][targetCoords[1]];
+        console.log({cost});
+        if(cost === lowestCost){
+            lowestMoves.push([...targetCoords]);
+        }
+        if(cost < lowestCost){
+            lowestMoves = [[...targetCoords]];
+            lowestCost = cost;
         }
         
-    return null
     }
-
+    if(getAtCoords(maze,currentCoords) !== "S"){
+    queue = [...lowestMoves, ...queue];
+    }
+    cells.push(...lowestMoves);
+}
+    
+const uniqCells = new Set(cells.map(c=>coordToString(c)));
+console.log(distanceArray);
+    return -1 
+}
 
 
 const main = async () =>{
-    const input = await readInput("./inputs/16.txt");
-    const maze = parseMaze(input);
-    console.log({globalDeer});
-    const res = solveOne(maze,globalDeer);
-    console.log({res, globalLowest});
+    const input = await readInput("./inputs/16.test.txt");
+    // let res = one(input);
+    // console.log({res});
+    let p2 = two(input);
+    console.log(JSON.stringify(p2,null,2));
 
 }
 
